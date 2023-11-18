60b65555b90ff468726d1dd2c5ff4f44b1373981
reemkarmi <reem.karmi02@gmail.com>
2023-11-18 10:24:57 +0300

    update

--- RSA.py
+++ RSA.py
@@ -1,31 +1,35 @@
-
-def generate_prime_number(length):
-
-
-    
- 
-def mod_inverse(key,phi):
-     publickey = pow(key, -1, phi)
-     return publickey
- 
-    
-def encrypte(plain_text,public_key,n):
-    cipher_text=plain_text + public_key % n
-    return cipher_text
-
-def decripte(cipher_text,public_key,n):
-    kprime=n-public_key
-    plain_text=cipher_text-public_key % n
-    return plain_text
+def generate_keys(p, q, e):
+    n = p * q
+    On = (p - 1) * (q - 1)
+    d = pow(e, -1, On)
+    return (hex(e), hex(n)), (hex(d), hex(On))
 
 
-A = generate_prime_number(1000)
-B = generate_prime_number(1000)
+def encrypt(text, public_key):
+    p1 = int(public_key[0], base=16)
+    p2 = int(public_key[1], base=16)
 
-def generatekey(p,q,key):
-    n=p*q #two prime number 
-    phi=(p-1)*(q-1) 
-    return key,n,phi
+    unicode_values = []
+    for char in text:
+        unicode_values.append(hex(pow(ord(char), p1, p2)))
 
-    
+    encrypted_text = ''.join(str(v) for v in unicode_values)
 
+    return encrypted_text
+
+
+def decrypt(text, private_key, public_key):
+    p1 = int(private_key[0], base=16)
+    p2 = int(public_key[1], base=16)
+
+    decrypted_text = text.split("0x")
+    decrypted_text = decrypted_text[1:]
+
+    final_text = ""
+    for char in decrypted_text:
+        char = int(char, base=16)
+        char = pow(int(char), p1, p2)
+        char = chr(char)
+        final_text += char
+
+    return final_text
